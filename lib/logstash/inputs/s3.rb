554a84b908e4990a937de2a7d4ef6310a00e0bea
Divya Devaraj <ddevaraj@godaddy.com>
2017-06-16 16:29:45 -0700

    Added support for gzip extension
    Resolves: https://github.com/logstash-plugins/logstash-input-s3/pull/115

--- lib/logstash/inputs/s3.rb
+++ lib/logstash/inputs/s3.rb
@@ -281,7 +281,7 @@ class LogStash::Inputs::S3 < LogStash::Inputs::Base
 
   private
   def gzip?(filename)
-    filename.end_with?('.gz')
+    filename.end_with?('.gz','.gzip')
   end
   
   private
