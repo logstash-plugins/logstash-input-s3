d31f5d95f723a0679ac9fd93bcdc5bd627fe70d5
Eric Herot <400447+eherot@users.noreply.github.com>
2023-11-20 17:44:15 -0500

    Test our additions to the poller

--- spec/inputs/s3/poller_spec.rb
+++ spec/inputs/s3/poller_spec.rb
@@ -41,5 +41,49 @@ describe LogStash::Inputs::S3::Poller do
     subject.stop
     wait_for { t.status }.to eq(false)
   end
-end
 
+  context 'use_start_after is true' do
+    let(:options) { {:use_start_after => true} }
+    subject { described_class.new(bucket, sincedb, logger, options) }
+
+    context 'there are files to fetch' do
+      it "sets the last_key_fetched" do
+        retrieved_objects = []
+
+        subject.run do |object|
+          retrieved_objects << object
+          subject.stop if objects.size == retrieved_objects.size
+        end
+
+        expect(subject.instance_variable_get(:@last_key_fetched)).to eq(objects.first.key)
+      end
+    end
+
+    context 'there are no files to fetch' do
+      let(:objects) { [] }
+
+      it "does not set the last_key_fetched" do
+        subject.run {}
+        expect(subject.instance_variable_get(:@last_key_fetched)).to be_nil
+      end
+
+      context 'and there is an oldest_key in the sincedb' do
+        let(:oldest_key) { "oldest-object-key" }
+
+        it 'calls @bucekt.objects with the appropriate listing options' do
+          allow(sincedb).to receive(:oldest_key).and_return(oldest_key)
+          subject.run {}
+          expect(bucket).to have_received(:objects).with({:start_after => oldest_key})
+        end
+      end
+
+      context 'and there is no oldest_key in the sincedb' do
+        it 'calls @bucket.objects with no listing options' do
+          allow(sincedb).to receive(:oldest_key).and_return(nil)
+          subject.run {}
+          expect(bucket).to have_received(:objects).with({})
+        end
+      end
+    end
+  end
+end
